afe0e8aa400b8095acd01f7522a0775ffddfbe98
anika-ani <varnitha08dinakar@gmail.com>
2025-04-17 13:35:36 +0530

    js files

---
+++ variables and data types.js
@@ -0,0 +1,10 @@
+let heading=document.querySelector(".chellakutty");
+console.dir(heading)
+
+let headings=document.querySelectorAll("#hey");
+console.dir(heading)
+
+
+// let headings = document.getElementsByTagName("p");
+// console.dir(headings);
+// console.log(headings[0]);
